addc61d67f55754e133850db388419f8836a14b0
oilybikechain <oilybikechaindeveloper@gmail.com>
2025-05-09 22:24:46 +0800

    Started on transactions page

--- lib/utilities/transactions_tile.dart
+++ lib/utilities/transactions_tile.dart
@@ -1,20 +1,70 @@
 import 'package:finance_tracker/data/transactions_class.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TransactionsTile extends StatelessWidget {
   final Transactions transactionData;
-  final VoidCallback? onEdit;
-  final VoidCallback? onDelete;
+  final Function(BuildContext) onEdit;
+  final Function(BuildContext) onDelete;
   
   const TransactionsTile({
     super.key,
     required this.transactionData,
-    this.onEdit,
-    this.onDelete,
+    required this.onEdit,
+    required this.onDelete,
   });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final textTheme =
+        Theme.of(context).textTheme;
+    final colorScheme =
+        Theme.of(context).colorScheme;
+
+
+    return Padding(
+      padding: const EdgeInsets.only(
+        right: 25.0,
+        left: 25.0,
+        top: 25.0,
+      ),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),  
+          children: [
+            SlidableAction(
+              onPressed: onDelete,
+              icon: Icons.delete,
+              backgroundColor:
+                  Colors.red.shade300,
+            ),
+            SlidableAction(
+              onPressed: onEdit,
+              icon: Icons.edit,
+              backgroundColor: Colors.blueGrey,
+            )
+
+          ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(
+              255,
+              94,
+              94,
+              94,
+            ),
+            borderRadius:
+                BorderRadius.circular(8),
+          ),
+          child: Row(
+            children: [
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
